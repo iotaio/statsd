ce3d87830a95ec2598f211ba6400440556016829
banyanleaf <chris@iota.io>
2015-06-10 06:25:18 -0400

    fixing path

--- bin/run_statsd.sh
+++ bin/run_statsd.sh
@@ -1,1 +1,1 @@
-/opt/iota/nodejs/node/bin/node /opt/iota/statsd/statsd/stats.js /opt/iota/iota/statsd/etc/config.js
+/opt/iota/nodejs/node/bin/node /opt/iota/statsd/statsd/stats.js /opt/iota/iota/statsd/config.js
