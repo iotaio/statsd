60bf93861e5893b14b2ccb93a8bff945372594ea
banyanleaf <chris@iota.io>
2015-06-10 06:06:43 -0400

    restructuring statsd

---
+++ bin/run_statsd.sh
@@ -0,0 +1,1 @@
+/opt/iota/nodejs/node/bin/node /opt/iota/statsd/statsd/stats.js /opt/iota/iota/statsd/etc/config.js
---
+++ etc/config.js
@@ -0,0 +1,8 @@
+{
+ debug: 'true'
+, iotaioPort: 80
+, iotaioApiKey: '123'
+, iotaioHost: '127.0.0.1'
+, iotaioTag: 'antimatter'
+, backends: [ './backends/iotaio' ]
+}
